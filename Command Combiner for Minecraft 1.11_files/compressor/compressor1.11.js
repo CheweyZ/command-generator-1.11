953431516efcebf4ac44eb8b335b8a3617fa2fec
Zak_Ki <cheweyzak002@gmail.com>
2016-12-30 15:28:40 +1300

    Base build for for adaptability on compressor

--- Command Combiner for Minecraft 1.11_files/compressor/compressor1.11.js
+++ Command Combiner for Minecraft 1.11_files/compressor/compressor1.11.js
@@ -1,49 +1,83 @@
 function compressor(check) {
     return check.map(function(current) {
-        var stonetype
-        //console.log(current);
-        return current.replace(/stone *variant=([^\s]*)/gi, (function(a, b) {
+        var itemtype
+        var reply
+            //console.log(current);
+            //
+            //Multipleprams detection   
+            // /stone ((?!\s{2}).)+
+            //Onecondition 
+            // /stone *variant=([^\s]*)/gi
+
+        current = current.replace(/((?=minecraft:stone )((?!\s{2}).)+|(?=stone )((?!\s{2}).)+)/gi, (function(a, b) {
             b = b.toLowerCase()
-            //console.log(b);
-            switch (b) {
+            console.log(b);
+            b.replace(/variant=([^\s]*)/gi, (function (c,d) {
+                //console.log(b);
+            switch (d) {
                 case 'stone':
-                    stonetype = 'stone'
+                    itemtype = 'stone'
                     break;
                 case 'granite':
-                    stonetype = 'stone 1'
+                    itemtype = 'stone 1'
                     break;
                 case 'smooth_granite':
                 case 'smooth granite':
                 case 'polished granite':
                 case 'polished_granite':
-                    stonetype = 'stone 2'
+                    itemtype = 'stone 2'
                     break;
                 case 'diorite':
-                    stonetype = 'stone 3'
+                    itemtype = 'stone 3'
                     break;
                 case 'smooth_diorite':
                 case 'smooth diorite':
                 case 'polished diorite':
                 case 'polished_diorite':
-                    stonetype = 'stone 4'
+                    itemtype = 'stone 4'
                     break;
                 case 'andesite':
-                    stonetype = 'stone 5'
+                    itemtype = 'stone 5'
                     break;
                 case 'smooth_andesite':
                 case 'smooth andesite':
                 case 'polished andesite':
                 case 'polished_andesite':
-                    stonetype = 'stone 6'
+                    itemtype = 'stone 6'
                     break;
                 case 'cobblestone':
-                    stonetype = 'cobblestone'
+                    itemtype = 'cobblestone'
                     break;
                 default:
-                    stonetype = 'stone'
+                    itemtype = 'stone'
             }
-            //console.log(stonetype);
-            return stonetype
+            //console.log(itemtype);
+            return itemtype
+          }))
+          return itemtype
         }))
+        current = current.replace(/((?=minecraft:stonebrick )((?!\s{2}).)+|(?=stonebrick )((?!\s{2}).)+)/gi, (function(a, b) {
+            b = b.toLowerCase()
+            //in event they ever update names
+            currentblock='stonebrick'
+            switch (b) {
+                case 'stonebrick':
+                    itemtype = currentblock
+                    break;
+                case 'mossy_stonebrick':
+                    itemtype = currentblock+ ' 1'
+                    break;
+                case 'cracked_stonebrick':
+                    itemtype = currentblock+' 2'
+                    break;
+                case 'chiseled_stonebrick':
+                    itemtype = 'cracked_stonebrick'
+                    break;
+                default:
+                    itemtype = 'stonebrick'
+            }
+            return itemtype
+        }))
+        return current
     })
 }
